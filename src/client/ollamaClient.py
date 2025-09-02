3a5ce2c17767eb1a0a17e735865b748cba85666c
Aadil Minhaz <aadilminhaz@gmail.com>
2025-09-02 13:27:42 +0100

    Updated Ollama Client

--- src/client/ollamaClient.py
+++ src/client/ollamaClient.py
@@ -1,12 +1,22 @@
 import ollama
 
-target_model = 'llama2'
+target_model = 'deepseek-r1'
 
     
-def call_target_model(prompt):
+def inference(prompt):
     response = ollama.generate(model=target_model, prompt=prompt)
     print(response['response'])
+    full_response = response['response']
 
-#call_target_model('What is capital of Japan')
+    # Remove the <think> section if present
+    if "<think>" in full_response and "</think>" in full_response:
+        # Extract everything after </think>
+        final_response = full_response.split("</think>")[-1].strip()
+    else:
+        final_response = full_response.strip()
+
+    
+    return final_response
+
 
 
